439aca09d93f3791256c7937e0b540ce9a76ff7c
balu perla <baluperla66@gmail.com>
2025-09-15 17:28:50 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -15,6 +15,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
   Form,
